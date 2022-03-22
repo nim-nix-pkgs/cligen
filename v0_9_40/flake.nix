d2b953c68dca8faa7216489f8dcf415359391017
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:18 -0300

    chore: re-index ../nimpkgs/c/cligen/v0_9_40

--- v0_9_40/flake.nix
+++ v0_9_40/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Infer & generate command-line interace/option/argument parsers'';
+  description = ''Infer & generate command-line interace/option/argument parser'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,8 +9,8 @@
   
   inputs.src-cligen-v0_9_40.flake = false;
   inputs.src-cligen-v0_9_40.owner = "c-blake";
-  inputs.src-cligen-v0_9_40.ref   = "refs/tags/v0.9.40";
-  inputs.src-cligen-v0_9_40.repo  = "cligen";
+  inputs.src-cligen-v0_9_40.ref   = "v0_9_40";
+  inputs.src-cligen-v0_9_40.repo  = "cligen.git";
   inputs.src-cligen-v0_9_40.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
